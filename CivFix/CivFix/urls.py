77739c330746e11f69045a385be060241a24299e
MaxNguyen5040 <MaxNguyen5040>
2023-11-04 20:53:23 -0400

    implemented auth0 functionality.

--- CivFix/CivFix/urls.py
+++ CivFix/CivFix/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-# from . import views
+
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls', namespace='users')),
-    path('tickets/', include('tickets.urls', namespace='tickets')),
-    # path('', views.index, name='home')
+    # path('tickets/', include('tickets.urls', namespace='tickets')),
+    # path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+    path("callback", views.callback, name="callback"),
+    # path('tickets/', include('tickets.urls')),
 ]
-
-
-
--- CivFix/users/urls.py
+++ CivFix/users/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-# from . import views
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
 ]
-
-
