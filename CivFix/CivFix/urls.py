5b19aec5d98413cc981ec3a1a1955a5db0394f9f
Ricky L. T. Wong <rickyltwong2@gmail.com>
2023-11-04 22:13:08 -0400

    clean codebase

--- CivFix/CivFix/urls.py
+++ CivFix/CivFix/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
-
 
 from users import views
 
@@ -10,8 +8,8 @@ urlpatterns = [
     # path('tickets/', include('tickets.urls', namespace='tickets')),
     # path('', views.index, name='index'),
     path("", views.index, name="index"),
-    path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout"),
+    path("login/", views.login, name='login'),
+    path("logout/", views.logout, name="logout"),
     path("callback", views.callback, name="callback"),
     # path('tickets/', include('tickets.urls')),
 ]
--- CivFix/users/urls.py
+++ CivFix/users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # user profile page
     path('id/<int:user_id>/', views.profile, name='profile'),
     path('leaderboard/', views.leaderboard, name='leaderboard'),
 ]
